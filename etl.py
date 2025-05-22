ab3fe77158bdccd9684c56daece55495173c0e9e
Randa Yasser <randa.yasser1999@gmail.com>
2025-05-22 10:57:38 +0300

    added transformation

--- etl.py
+++ etl.py
@@ -8,3 +8,15 @@ import pandas as pd
 df = pd.read_csv("raw_data.csv")
 
 #################################################################################
+# Commit 2: Transform
+
+# Normalize column names
+df.columns = [col.lower().replace(" ", "_") for col in df.columns]
+
+# Handle missing values
+df.fillna({"name": "Unknown", "age": df["age"].median(), "purchase_amount": 0}, inplace=True)
+
+# Convert purchase date to uniform format
+df["purchasedate"] = pd.to_datetime(df["purchasedate"], errors="coerce")
+
+#################################################################################
