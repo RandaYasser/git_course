cdf445f597e01e5e13eca835e8f926a582f0379f
Randa Yasser <randa.yasser1999@gmail.com>
2025-05-22 11:07:02 +0300

    added load

--- etl.py
+++ etl.py
@@ -20,3 +20,8 @@ df.fillna({"name": "Unknown", "age": df["age"].median(), "purchase_amount": 0}, 
 df["purchasedate"] = pd.to_datetime(df["purchasedate"], errors="coerce")
 
 #################################################################################
+
+# Commit 3: Load
+
+# Save the final processed data into a local file
+df.to_csv("final_data.csv", index=False)
